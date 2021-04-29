edd0327beda01967a9e07bf0f7488c6169c3d7b8
marcelo.dias <marcelovianadias@outlook.com>
2021-04-29 16:48:24 -0300

    leitura de todos os livros por categoria implementado

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Livro } from '../livro.model';
+import { LivroService } from '../livro.service';
 
 @Component({
   selector: 'app-livro-read-all',
@@ -9,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
 export class LivroReadAllComponent implements OnInit {
 
   displayedColumns : String[] = [ 'id' , 'titulo' , 'livros', 'acoes' ];
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  id_cat: String = ''
+  
+  livros: Livro[] = []
+
+  constructor(private service: LivroService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {  
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.findAll()
+  }
+
+  findAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
+      this.livros = resposta;
+      console.log(resposta)
+
+    })
   }
 
 }
