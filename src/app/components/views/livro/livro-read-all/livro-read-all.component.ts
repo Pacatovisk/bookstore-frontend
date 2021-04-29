82f6266b646f80720765b9acd3866965abc5adc6
marcelo.dias <marcelovianadias@outlook.com>
2021-04-29 16:33:47 -0300

    implementando component read para livros

---
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css'
+  ]
+})
+export class LivroReadAllComponent implements OnInit {
+
+  displayedColumns : String[] = [ 'id' , 'titulo' , 'livros', 'acoes' ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
